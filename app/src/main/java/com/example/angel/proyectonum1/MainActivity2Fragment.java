0a3f716fdcd8741a46e18b58fd24da1bf5cd58f3
angel <agomezcastro@danielcastelao.com>
2017-01-16 20:39:52 +0100

    codigo actualizado

--- app/src/main/java/com/example/angel/proyectonum1/MainActivity2Fragment.java
+++ app/src/main/java/com/example/angel/proyectonum1/MainActivity2Fragment.java
@@ -4,5 +4,46 @@ package com.example.angel.proyectonum1;
  * Created by angel on 15/01/2017.
  */
 
-public class MainActivity2Fragment {
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
+
+public class MainActivity2Fragment extends Fragment {
+
+    public MainActivity2Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layFrag2 = inflater.inflate(R.layout.fragment_main2, container, false);
+
+        TextView text2 = (TextView) layFrag2.findViewById(R.id.text2);
+
+        String str = getActivity().getIntent().getStringExtra("Llamar");
+
+        text2.setText(str);
+
+        Button button2 = (Button) layFrag2.findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent databack = new Intent();
+                databack.putExtra("Llamar2", "mensaje desde fragment2");
+
+                setResult(RESULT_OK,databack);
+                finish();
+
+            }
+        });
+        return layFrag2;
+    }
 }
--- app/src/main/java/com/example/angel/proyectonum1/MainActivityFragment.java
+++ app/src/main/java/com/example/angel/proyectonum1/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.example.angel.proyectonum1;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public final static int CODIGO=1;
+    private TextView text;
 
     public MainActivityFragment() {
     }
@@ -18,5 +26,18 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CODIGO) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra("Llamar2");
+                text.setText(result);
+            }
+        }
     }
 }
